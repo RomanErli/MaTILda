6cc1a7ffc7be8c81904164d2cf77b03b949d79d6
Roman <roman@erlmoser.com>
2018-04-04 23:16:03 +0200

    Umgestellt auf mongoDB

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/matilda';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,8 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/events', events)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,10 @@ module.exports = router;
 
 function  setResHtml(cb) {};
 
-var reo ='<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
+//var reo ='<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
+
+/*
+der Teil macht mysql
 
 var dbcon_query = require("../dbConnection")
 dbcon_query('select * from matildadb.v_lager' // where ?', {idT_Adr: '=>1'}
@@ -16,21 +19,29 @@ dbcon_query('select * from matildadb.v_lager' // where ?', {idT_Adr: '=>1'}
         {
             throw (err);
     } else
- {
-     /* GET home page. */
-     router.get('/', function(req, res, next) {
-         //res.render('index', {title: 'Matilda'});
-         obj = {print: result, title: 'Matilda'};
-         res.render('index', obj);
-         //console.log(result);
-     });
+ */
 
-     router.post('/insert', function (req, res, next) {
+// ab hier ist mongoose
 
+//Import the mongoose module
+var mongoose = require('mongoose');
 
-     } )
- }
-         });
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+{
+    /* GET home page. */
+    router.get('/', function (req, res, next) {
+        //res.render('index', {title: 'Matilda'});
+        obj = {print: result, title: 'Matilda'};
+        res.render('index', obj);
+        //console.log(result);
+    });
+};
+
 
 
 
